de346f76b994cce314a971ffe5dd924e18308b15
ajrpTec <ajrp@tec.dk>
2021-10-16 09:47:54 +0200

    ændrer variabelnavne til engelsk og formaterer koden

--- HandIn1.java
+++ HandIn1.java
@@ -1,61 +1,71 @@
 import java.util.Scanner;
 import java.util.Arrays;
 
-class HandIn1{
+class HandIn1 {
 
+  //By klasse - der samler data for hver by
+  private static class Town implements Comparable<Town> {
+    String  name;
+    int     customers;
+    int     accumulatedCustomerDistance;
 
-    private static class By implements Comparable<By>{
-      String navn;	int kunder; int samletAfstandTilKunder;
+    Town(String name, int customers) {
+      this.name       = name;
+      this.customers  = customers;
+    }
 
-      By(String navn, int kunder){this.navn = navn; this.kunder	= kunder;}
+    public int compareTo(Town by) {
+      if (by.accumulatedCustomerDistance > accumulatedCustomerDistance)
+        return -1;
+      else if (by.accumulatedCustomerDistance < accumulatedCustomerDistance)
+        return 1;
+      else
+        return 0;
+    }
 
-      public int compareTo(By byer){
-        if(byer.samletAfstandTilKunder      > samletAfstandTilKunder)      return -1;
-        else if(byer.samletAfstandTilKunder < samletAfstandTilKunder)	     return 1;
-        else  return 0;
+    public void addCustomerDistance(Town town, int distanceToTown) {
+      accumulatedCustomerDistance += town.customers * distanceToTown;
+    }
+  }
+
+  //Inverted Insertion sort - dvs. mindste først
+  public static void invertedInsertionSort(Comparable[] a) {
+    int N = a.length;
+    for (int i = 1; i < N; i++) {
+      for (int j = i; j > 0 && a[j - 1].compareTo(a[j]) > 0; j--) {
+        Comparable t = a[j];
+        a[j] = a[j - 1];
+        a[j - 1] = t;
       }
+    }
+  }
 
-      public void addAfstandTilKunder(By byer, int afstandTilBy){samletAfstandTilKunder += byer.kunder*afstandTilBy;}
+  public static void main(String[] args) {
+    Scanner s     = new Scanner(System.in);
+    int N         = s.nextInt();
+    Town[] towns  = new Town[N];
+
+    // Indlæser bynavn og antal kunder og opretter by-objekter
+    for (int i = 0; i < N; i++) {
+      towns[i] = new Town(s.next(), s.nextInt());
     }
 
-
-    public static void invertedInsertionSort(Comparable[] a){
-      int N = a.length;
-        for (int i = 1; i < N; i ++){
-            for (int j = i; j > 0 && a[j-1].compareTo(a[j]) > 0 ; j--){
-              Comparable t = a[j]; a[j] = a[j-1]; a[j-1] = t;
-            }
-          }
-    }
-
-
-    public static void main(String[] args){
-      Scanner s = new Scanner(System.in);
-      int N     = s.nextInt();
-      By[] byer   = new By[N];
-
-
-    //Indlæser bynavn og antal kunder og opretter byer-objekter
-    for(int i = 0; i < N; i++){
-      byer[i] = new By(s.next(),s.nextInt());
-    }
-
-
-    //Indlæser afstande imellem byer og beregner samletAfstandTilKunder
+    // Indlæser afstande imellem by og beregner samletAfstandTilKunder
     s.nextLine();
-    for(int r = 0 ; r < N ; r ++){
-      for(int c = 0; c < r ; c ++){
+    for (int r = 0; r < N; r++) {
+      for (int c = 0; c < r; c++) {
         int afstand = s.nextInt();
-         byer[c].addAfstandTilKunder( byer[r],afstand);
-         byer[r].addAfstandTilKunder( byer[c],afstand);
+        towns[c].addCustomerDistance(towns[r], afstand);
+        towns[r].addCustomerDistance(towns[c], afstand);
       }
       s.nextLine();
     }
 
-    //Sorterer byerne på baggrund af summeret afstand til kunder, mindste først
-    invertedInsertionSort(byer);
+    // Sorterer byne på baggrund af summeret afstand til kunder, mindste først
+    invertedInsertionSort(towns);
 
-    for(By by: byer)System.out.println(by.navn);
+    for (Town town : towns)
+      System.out.println(town.name);
   }
 
 }
